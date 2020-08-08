faf83dba5a3a3d70b53b6da03960dbd23a9e331f
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-08-08 17:50:16 +0530

    updated django static

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 
 from personal.views import (
     home_screen_view,
@@ -43,3 +44,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
