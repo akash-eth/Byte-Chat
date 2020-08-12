2948704dcc8ea55f6127740b684420e36ecbe7bc
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-08-12 10:01:02 +0530

    added update blog view

--- blog/views.py
+++ blog/views.py
@@ -33,3 +33,36 @@ def detail_blog_view(request, slug):
 	context['blog_post'] = blog_post
 
 	return render(request, 'blog/detail_blog.html', context)
+
+
+def edit_blog_view(request, slug):
+
+	context = {}
+
+	user = request.user
+	if not user.is_authenticated:
+		return redirect("must_authenticate")
+
+	blog_post = get_object_or_404(BlogPost, slug=slug)
+
+	if blog_post.author != user:
+		return HttpResponse('You are not the author of that post.')
+
+	if request.POST:
+		form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=blog_post)
+		if form.is_valid():
+			obj = form.save(commit=False)
+			obj.save()
+			context['success_message'] = "Updated"
+			blog_post = obj
+
+	form = UpdateBlogPostForm(
+			initial = {
+					"title": blog_post.title,
+					"body": blog_post.body,
+					"image": blog_post.image,
+			}
+		)
+
+	context['form'] = form
+	return render(request, 'blog/edit_blog.html', context)
